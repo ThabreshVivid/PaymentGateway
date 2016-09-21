df05bee08e44c2630e4bc48577c3b12fad289a65
ThabreshVivid <subramanibr@thabresh.local>
2016-09-21 12:22:04 +0530

    Initial Commit

---
+++ PaymentGateway/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaymentGateway
+//
+//  Created by Thabresh on 9/21/16.
+//  Copyright © 2016 VividInfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PayPalMobile.h"
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PaymentGateway/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PaymentGateway
+//
+//  Created by Thabresh on 9/21/16.
+//  Copyright © 2016 VividInfotech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

5b211af428ba45ca3ed9d57c8974273b383d81f1
ThabreshVivid <subramanibr@thabresh.local>
2016-09-21 12:30:22 +0530

    updated client id

--- PaymentGateway/AppDelegate.m
+++ PaymentGateway/AppDelegate.m
@@ -18,18 +18,19 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [PayPalMobile initializeWithClientIdsForEnvironments:@{PayPalEnvironmentProduction : @"YOUR_CLIENT_ID_FOR_PRODUCTION",
-            PayPalEnvironmentSandbox : @"AXKxccEA8keRFMMcfO1gUPuGs6N6O8kmq0BUEu5RoE_CEraXSWWwdgmOA2CXvQ9BfxVbCm_KvuUGLvYA"}];
+            PayPalEnvironmentSandbox : @"YOUR_CLIENT_ID_FOR_SANDBOX"}];
+    
     // Override point for customization after application launch.
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as [AXKxccEA8keRFMMcfO1gUPuGs6N6O8kmq0BUEu5RoE] incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your [_CEraXSWWwdgmOA2CXvQ9BfxVbCm_KvuUGLvYA]application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
